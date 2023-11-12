fc0ae8cedbb000b17f64796ea05f3993389bdb5c
rayeensaniya <149939014+rayeensaniya@users.noreply.github.com>
2023-11-12 02:06:54 -0800

    todo detail and add screen

--- lib/screens/add_screen.dart
+++ lib/screens/add_screen.dart
@@ -5,11 +5,12 @@ import 'package:todo_app/widget/button.dart';
 import 'package:todo_app/widget/text_field.dart';
 
 class AddScreen extends StatefulWidget {
-   AddScreen({Key? key}) : super(key: key);
+  const AddScreen({Key? key}) : super(key: key);
 
   @override
   State<AddScreen> createState() => _AddScreenState();
-  }
+}
+
 final _formKey = GlobalKey<FormState>();
 final _addBloc = AddScreenBloc();
 
@@ -25,13 +26,15 @@ class _AddScreenState extends State<AddScreen> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-            padding:  EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(20.0),
             child: Form(
               key: _formKey,
               child: Column(
                 children: [
-                   AppTextField(labelText: 'TITLE', maxLines: 2,
-                  validator: (p0) => _addBloc.titleValidator(title: p0),
+                  AppTextField(
+                    labelText: 'TITLE',
+                    maxLines: 2,
+                    validator: (p0) => _addBloc.titleValidator(title: p0),
                   ),
                   const SizedBox(
                     height: 20,
@@ -45,26 +48,26 @@ class _AddScreenState extends State<AddScreen> {
                     height: 20,
                   ),
                   AppTextField(
-
                     labelText: 'Due Date',
                     controller: dateController,
                     readOnly: true,
                     filled: true,
-                    onTp: () async{
-                   final dateTime =await _selectDate();
-                      String dateFormatted = DateFormat('dd/MM/yyyy').format(dateTime!);
-                      dateController.text=dateFormatted;
+                    onTp: () async {
+                      final dateTime = await _selectDate();
+                      String dateFormatted =
+                          DateFormat('dd/MM/yyyy').format(dateTime!);
+                      dateController.text = dateFormatted;
                     },
                     suffixIcon: const Icon(Icons.calendar_today),
                   ),
                   const SizedBox(
                     height: 60,
                   ),
-                  AppButton(onpPressed: () {
-                    if(_formKey.currentState!.validate()){
-
-                    }
-                  }, text: 'Submit')
+                  AppButton(
+                      onpPressed: () {
+                        if (_formKey.currentState!.validate()) {}
+                      },
+                      text: 'Submit')
                 ],
               ),
             )),
@@ -73,16 +76,12 @@ class _AddScreenState extends State<AddScreen> {
   }
 
   Future<DateTime?> _selectDate() async {
-    DateTime? _picked = await showDatePicker(
+    DateTime? picked = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
       firstDate: DateTime(2000),
       lastDate: DateTime(2500),
     );
-    if (_picked != null) {
-     return _picked;
-
-    }
-
+    return picked;
   }
 }
